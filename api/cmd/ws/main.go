fdbfac3a8d232f7d4dbfc83500c5379742d27c74
Pedro Henrique Bérgamo <pedroh.bergamo20@gmail.com>
2024-08-05 16:07:20 -0300

    ✨ feat: Aula 01

---
+++ api/cmd/ws/main.go
@@ -0,0 +1,56 @@
+package main
+
+import (
+	"ask-me-anything/internal/api"
+	"ask-me-anything/internal/pgstore"
+	"context"
+	"errors"
+	"fmt"
+	"net/http"
+	"os"
+	"os/signal"
+
+	"github.com/jackc/pgx/v5/pgxpool"
+	"github.com/joho/godotenv"
+)
+
+func main() {
+	if err := godotenv.Load(); err != nil {
+		panic(err)
+	}
+
+	ctx := context.Background()
+
+	pool, err := pgxpool.New(ctx, fmt.Sprintf(
+		"user=%s password=%s host=%s port=%s dbname=%s",
+		os.Getenv("PG_USER"),
+		os.Getenv("PG_PASSWORD"),
+		os.Getenv("PG_HOST"),
+		os.Getenv("PG_PORT"),
+		os.Getenv("PG_DATABASE"),
+	))
+
+	if err != nil {
+		panic(err)
+	}
+
+	defer pool.Close()
+
+	if err := pool.Ping(ctx); err != nil {
+		panic(err)
+	}
+
+	handler := api.NewHandler(pgstore.New(pool))
+
+	go func() {
+		if err := http.ListenAndServe(":3333", handler); err != nil {
+			if !errors.Is(err, http.ErrServerClosed) {
+				panic(err)
+			}
+		}
+	}()
+
+	quit := make(chan os.Signal, 1)
+	signal.Notify(quit, os.Interrupt)
+	<-quit
+}
